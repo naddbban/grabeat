c98e8b843c9372b3ecdbc846d775eb47d49089d0
xxxsav <42399823+xxxsav@users.noreply.github.com>
2021-01-12 03:02:47 +0700

    Add files via upload

---
+++ grabeat/Controller/PromoController.cs
@@ -0,0 +1,27 @@
+﻿using grabeat.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace grabeat.Controller
+{
+    public class PromoController
+    {
+        public List<Diskon> diskon;
+
+        public PromoController()
+        {
+            diskon = new List<Diskon>();
+        }
+
+        public void addPromo(Diskon diskon)
+        {
+            this.diskon.Add(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.diskon;
+        }
+    }
+}
